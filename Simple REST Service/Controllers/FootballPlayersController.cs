5db37744d17eb574a9e4c4b9109b6f787fc9d83b
MikkelVJ <73933137+MikkelVJ@users.noreply.github.com>
2021-11-07 02:43:29 +0100

    Add files via upload

---
+++ Simple REST Service/Controllers/FootballPlayersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FootballPlayer;
+using Simple_REST_Service.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Simple_REST_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FBPlayersController : ControllerBase
+    {
+        private readonly FootBallPlayersManager _manager = new FootBallPlayersManager();
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<FBPlayer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public FBPlayer Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public FBPlayer Post([FromBody] FBPlayer value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public FBPlayer Put(int id, [FromBody] FBPlayer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public FBPlayer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ Simple REST Service/Managers/FootBallPlayersManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FootballPlayer;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Simple_REST_Service.Managers
+{
+    public class FootBallPlayersManager
+    {
+        private static int _nextId = 1;
+
+        private static readonly List<FBPlayer> Data = new List<FBPlayer>()
+        {
+            new FBPlayer("john", 13, _nextId++, 13),
+            new FBPlayer("jen", 31, _nextId++, 31)
+        };
+
+        public List<FBPlayer> GetAll()
+        {
+            return new List<FBPlayer>(Data);
+        }
+
+        public FBPlayer GetById(int id)
+        {
+            return Data.Find(Item => Item.Id == id);
+        }
+
+        public FBPlayer Add(FBPlayer newFBPlayer)
+        {
+            newFBPlayer.Id = _nextId++;
+            Data.Add(newFBPlayer);
+            return newFBPlayer;
+        }
+
+        public FBPlayer Delete(int id)
+        {
+            FBPlayer player = Data.Find(Player1 => Player1.Id == id);
+            if (player == null) return null;
+            Data.Remove(player);
+            return player;
+        }
+
+        public FBPlayer Update(int id, FBPlayer update)
+        {
+            FBPlayer player = Data.Find(player => player.Id == id);
+            if (player == null) return null;
+            player.Name = update.Name;
+            player.ShirtNumber = update.ShirtNumber;
+            player.price = update.price;
+            return player;
+        }
+    }
+}
